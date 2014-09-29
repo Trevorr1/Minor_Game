a82abb4d5d8a72c79bc9127560c45c9b4eba768f
Gijs-W <gijsvandewater+github@gmail.com>
2014-09-29 14:15:10 +0200

    MoveComponent

--- Overdose/Overdose/Main.cpp
+++ Overdose/Overdose/Main.cpp
@@ -10,15 +10,18 @@ int main(int argc, const char* argv[])
 
 	//moet ff uitzoeken hoe de componentContainer benaderbaar is via GameEntitty
 	GameEntity *entity = new GameEntity;
-
-
-
 	entity->addComponent(*new DummyComponent);
-	entity->addComponent(*new DummyComponent);
-	entity->tick();
+	entity->addComponent(*new MoveComponent);
 
-	
-	system("pause");
+	// hier later nog een keer een gameloop met dt enzo
+	while (true) {
+
+		
+		entity->tick();
+
+		std::cout << "PosX " << entity->getPosX() << "PosY " << entity->getPosY() << std::endl;
+		system("pause");
+	}
 
 	return 0;
 }
