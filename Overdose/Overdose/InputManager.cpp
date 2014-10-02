b2a08af629a38cdf1456cc73e3d79030257f9b0c
Gijs-W <gijsvandewater+github@gmail.com>
2014-10-02 20:12:16 +0200

    InputManager now supports integer for keycodes

--- Overdose/Overdose/InputManager.cpp
+++ Overdose/Overdose/InputManager.cpp
@@ -7,12 +7,12 @@ InputManager::InputManager()
 
 }
 
-void InputManager::addKeyPress(string keyPressed) {
+void InputManager::addKeyPress(int keyPressed) {
 	keyBuffer->push(keyPressed);
 }
 
-string InputManager::getLastKeyPress() {
-	string keyPress = "";
+int InputManager::getLastKeyPress() {
+	int keyPress = -1;
 
 	if (!keyBuffer->empty()) {
 		keyPress = keyBuffer->top();
--- Overdose/Overdose/InputManager.h
+++ Overdose/Overdose/InputManager.h
@@ -1,21 +1,20 @@
 #pragma once
 #include "stdafx.h"
 
-using std::string;
 using std::stack;
 namespace overdose {
 	class InputManager
 	{
 	protected:
 		// https://wiki.libsdl.org/SDL_Keycode
-		stack<string> *keyBuffer = new stack<string>();
+		stack<int> *keyBuffer = new stack<int>();
 
 	public:
 		InputManager();
-		~InputManager();
+		virtual ~InputManager();
 
-		void addKeyPress(string keyPressed);
-		string getLastKeyPress();
+		void addKeyPress(int keyPressed);
+		int getLastKeyPress();
 	private:
 		void clearKeyBufferButOne();
 	};
