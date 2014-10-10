d2721b44d9d1e2dc71bf4fdda2d6981d21900ef9
Gijs-W <gijsvandewater+github@gmail.com>
2014-10-10 15:14:19 +0200

    Revert "Revert "update""
    This reverts commit 07a3796143a9587e1208606223dd5845a86bb6e0.

--- Overdose/Overdose/EnemyMoveComponent.cpp
+++ Overdose/Overdose/EnemyMoveComponent.cpp
@@ -12,17 +12,15 @@ EnemyMoveComponent::~EnemyMoveComponent()
 
 }
 
-void  EnemyMoveComponent::receive(Component *subject, int message, GameEntity *object) 
+void  EnemyMoveComponent::receive(Component *subject, ComponentMessage message, GameEntity *object)
 {
 
 }
 
-void  EnemyMoveComponent::tick(GameEntity *entity)
+void  EnemyMoveComponent::tick(float dt, GameEntity *entity)
 {
 	if (entity->getPosX() < 0 || entity->getPosX() > 500)
 	{
 		entity->setSpeedX(entity->getSpeedX() * -1);
 	}
 }
-
-
--- Overdose/Overdose/Factory.cpp
+++ Overdose/Overdose/Factory.cpp
@@ -1,11 +1,12 @@
 #include "Factory.h"
 #include "MoveComponent.h"
+#include "DummyComponent.h"
 #include "EnemyMoveComponent.h"
 #include "CollisionComponent.h"
-#include "DummyComponent.h"
 #include "DrawComponent.h"
+#include "FPSDrawComponent.h"
 #include "PlayerInputComponent.h"
-
+#include <stdexcept>
 using namespace overdose;
 
 GameEntityFactory* GameEntityFactory::_instance = nullptr;
@@ -27,20 +28,26 @@ GameEntity* GameEntityFactory::getGameEntity(eGameEntity entityEnum){
 
 	switch (entityEnum){
 	case Policeman:
-		newObject->addComponent(*new EnemyMoveComponent());
-		newObject->addComponent(*new MoveComponent());
-		newObject->addComponent(*new DrawComponent("assets/sprites/cop.png", 1));
-		newObject->addComponent(*new CollisionComponent());
+		newObject->addComponent(new EnemyMoveComponent());
+		newObject->addComponent(new MoveComponent());
+		newObject->addComponent(new DrawComponent("assets/sprites/cop.png", 1));
+		newObject->addComponent(new CollisionComponent());
 		newObject->setSpeedX(-1);
 		break;
 	case Drugdealer:
-	//	newObject->addComponent(*new DummyComponent());
+		//	newObject->addComponent(*new DummyComponent());
+		break;
+	case FPSCounter:
+		newObject->addComponent(new FPSDrawComponent());
 		break;
 	case DrugAddict:
-		newObject->addComponent(*new PlayerInputComponent());
-		newObject->addComponent(*new MoveComponent());
-		newObject->addComponent(*new DrawComponent("assets/bottom_enemy_anim.tga", 3));
+		newObject->addComponent(new PlayerInputComponent());
+		newObject->addComponent(new MoveComponent());
+		newObject->addComponent(new DrawComponent("assets/bottom_enemy_anim.tga", 3));
+
 		break;
+	default:
+		throw std::invalid_argument("Invalid game Entity passed to the factory"); // Veel te lang moeten debuggen waarom mij entities geen components hadden... >.<
 	}
 
 	return newObject;
