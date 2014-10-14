e2e3cba8506ef8fa6980692c769739defa960f7b
Trevorr Marshall <fox4life76@gmail.com>
2014-10-14 13:14:52 +0200

    Collision now react to terrain and other entities differently...for the player anyway

--- Overdose/Overdose/CollisionReactionComponent.cpp
+++ Overdose/Overdose/CollisionReactionComponent.cpp
@@ -17,6 +17,7 @@ void CollisionReactionComponent::receive(Component *subject, ComponentMessage me
 	printf("CollisioReaction received");
 	
 	// resolve the collisions here
+	// use object to determine with what this collided
 	switch (message){
 	case PlayerInputComponent_ATTACK:
 		printf("Attacked by player");
@@ -36,13 +37,29 @@ void CollisionReactionComponent::receive(Component *subject, ComponentMessage me
 	case CollissionComponent_COLLISION_RIGHT:
 		printf("Collided with something from the right");
 		collidedX = true;
-		break; 
+		break;
+	case CollissionComponent_REACTION_TOP:
+		printf("Reacted with something from the top");
+		reactTop = true;
+		break;
+	case CollissionComponent_REACTION_BOTTOM:
+		printf("Reacted with something from the bottom");
+		reactBottom = true;
+		break;
+	case CollissionComponent_REACTION_LEFT:
+		printf("Reacted with something from the left");
+		reactX = true;
+		break;
+	case CollissionComponent_REACTION_RIGHT:
+		printf("Reacted with something from the right");
+		reactX = true;
+		break;
 	case Player_ATTACKING:
 		printf("Player_ATTAKING?");
 		isAlive = false;
 		break; 
 	defualt:
-
+		// do nothing for now
 		break;
 	}
 }
