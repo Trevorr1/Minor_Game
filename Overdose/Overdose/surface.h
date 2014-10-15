67f5b330a9dfe6059a7e181ea744131da044635f
basterbogt <bas@basterbogt.com>
2014-10-15 18:03:02 +0200

    Animation
    Animation

--- Overdose/Overdose/surface.h
+++ Overdose/Overdose/surface.h
@@ -134,6 +134,7 @@
 		void DrawScaled(int a_X, int a_Y, int a_Width, int a_Height, Surface* a_Target);
 		void SetFlags(unsigned int a_Flags) { m_Flags = a_Flags; }
 		void SetFrame(unsigned int a_Index) { m_CurrentFrame = a_Index; }
+		unsigned int GetCurrentFrame() { return m_CurrentFrame; }
 		unsigned int GetFlags() const { return m_Flags; }
 		int GetWidth() { return m_Width; }
 		int GetHeight() { return m_Height; }
