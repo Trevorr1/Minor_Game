8fb94ce89625e5210e86c0369ddfa4c9f6b44296
Gijs-W <gijsvandewater+github@gmail.com>
2014-11-13 13:21:32 +0100

    Added more parameters to ParticleComponent

--- Overdose/Overdose/ParticleComponent.cpp
+++ Overdose/Overdose/ParticleComponent.cpp
@@ -2,18 +2,20 @@
 
 using namespace overdose;
 
-ParticleComponent::ParticleComponent(float maxDt) {
+ParticleComponent::ParticleComponent(ParticleType type, float maxDt, float lifetime) {
 	m_maxDt = maxDt;
+	m_type = type;
+	m_lifetime = lifetime;
 }
 
 void ParticleComponent::createParticleFor(GameEntity *entity) {
 	/* misschien in factory stoppen? Idk D:*/
 	std::map<eAnimationState, Animation*>* animations = new std::map<eAnimationState, Animation*>();
-	animations->insert({ Default, new Animation("assets/sprites/smileyface.png", 1) });
+	animations->insert({ Default, new Animation(getSpritePath(), 1) });
 	DrawComponent *animation = new DrawComponent(animations);
 	animation->setAnimation(Default);//set starting animation
 
-	GameEntity *particle = new GameEntity(Particle, new MoveComponent, new KillSwitchComponent(0.5), animation, FinalComponent);
+	GameEntity *particle = new GameEntity(Particle, new MoveComponent, new KillSwitchComponent(m_lifetime), animation, FinalComponent);
 	particle->setPosY(entity->getPosY() + rand() % 10); // netter als we het niet op de game entity laten spawnen?
 	particle->setPosX(entity->getPosX() + rand() % 10);
 
@@ -21,6 +23,14 @@ void ParticleComponent::createParticleFor(GameEntity *entity) {
 	particle->setSpeedY(-250 + rand() % 500);
 
 	LevelManager::getInstance().getCurrentLevel()->scheduleEntityForInsertion(particle);
+}
+
+char* ParticleComponent::getSpritePath() {
+	switch (m_type) {
+	case SmileyFace:
+	default:
+		return "assets/sprites/smileyface.png";
+	}
 }
 
 void ParticleComponent::tick(float dt, GameEntity *entity) {
