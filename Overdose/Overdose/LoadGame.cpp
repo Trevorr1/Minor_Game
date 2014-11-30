3a4a15a866c2b82fa7b153a11a397371f487848e
Gijs-W <gijsvandewater+github@gmail.com>
2014-11-30 16:41:31 +0100

    Back to main menu

--- Overdose/Overdose/LoadGame.cpp
+++ Overdose/Overdose/LoadGame.cpp
@@ -4,6 +4,7 @@
 #include "MouseOverEffectComponent.h"
 #include "ButtonClickableReactionComponent.h"
 #include "ClickableComponent.h"
+#include "GameEntityFactory.h"
 using namespace overdose;
 
 LoadGame::LoadGame() {
@@ -20,29 +21,46 @@ void LoadGame::Init() {
 	vector<string> gameList = SaveGameManager::getInstance().getGameList();
 	int posY = 40;
 
-	TextGameEntity *mainText = new TextGameEntity("please select a saved game below");
-	mainText->setPosY(10);
-	mainText->setPosX(300);
-	addEntities(mainText);
-	for (string name : gameList) {
 
-		string prefix = "Savegame";
-	///	prefix.append(name);
-		//char* content = const_cast<char*>(prefix.c_str());
-		
-		TextGameEntity* text = new TextGameEntity("save game");
+	if (gameList.size() > 0) {
 
-		SaveGame game = SaveGameManager::getInstance().load(name);
+		TextGameEntity *mainText = new TextGameEntity("please select a saved game below");
+		mainText->setPosY(10);
+		mainText->setPosX(300);
+		addEntities(mainText);
 
-		text->addComponent(new MouseOverEffectComponent);
-		text->addComponent(new ClickableComponent);
-		text->addComponent(new ButtonClickableReactionComponent(game.savedLevelId));
+	
+		for (string name : gameList) {
 
-		text->setPosX(400);
-		text->setPosY(posY);
-		text->setHeight(15);
-		text->setWidth(100); // nodig voor MouseOverComponet
-		posY += 40;
-		addEntities(text);
+			string prefix = "Savegame";
+			///	prefix.append(name);
+			//char* content = const_cast<char*>(prefix.c_str());
+
+			TextGameEntity* text = new TextGameEntity("save game");
+
+			SaveGame game = SaveGameManager::getInstance().load(name);
+
+			text->addComponent(new MouseOverEffectComponent);
+			text->addComponent(new ClickableComponent);
+			text->addComponent(new ButtonClickableReactionComponent(game.savedLevelId));
+
+			text->setPosX(400);
+			text->setPosY(posY);
+			text->setHeight(15);
+			text->setWidth(100); // nodig voor MouseOverComponet
+			posY += 40;
+			addEntities(text);
+		}
 	}
+	else {
+		TextGameEntity *noSavedGames = new TextGameEntity("no saved games found");
+		noSavedGames->setPosY(10);
+		noSavedGames->setPosX(330);
+		addEntities(noSavedGames);
+	}
+
+	GameEntity* button = GameEntityFactory::getInstance().getGameEntity(ButtonMainMenuGreen);
+	button->setPosX(320);
+	button->setPosY(380);
+	addEntities(button);
 }
