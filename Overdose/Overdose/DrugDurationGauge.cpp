fd315fd771aa8996d81145de6f530bb043af2d28
Huan Nguyen <Huan500@hotmail.com>
2014-12-03 16:40:04 +0100

    DrugGauge lil fix

--- Overdose/Overdose/DrugDurationGauge.cpp
+++ Overdose/Overdose/DrugDurationGauge.cpp
@@ -11,12 +11,16 @@ DrugDurationGauge::DrugDurationGauge(int x, int y)
 	//m_DrawComponent = new DrawComponent(animations);
 	//m_DrawComponent->setAnimation(Default);//set starting animation
 	//addComponent(m_DrawComponent);
+	m_DrawComponent = nullptr;
 	m_Bar = DrawManager::getInstance().getCameraSurface(); //getSurface()
 }
 
 
 DrugDurationGauge::~DrugDurationGauge()
 {
+	if (m_DrawComponent != nullptr){
+		delete m_DrawComponent;
+	}
 	m_Bar = nullptr;
 	std::cout << "Deleted DrugDurationGauge\n";
 }
@@ -44,6 +48,34 @@ void DrugDurationGauge::tick(float dt)
 		break;
 	}
 
+	//std::cout << m_State << std::endl;
 	//m_Bar->Bar(2+x, y+4 + 64*(m_GaugeValue / FULL), 8+x, 64 + y, color); //Increase the bar height
 	m_Bar->Bar(1 + x, y-1 + 64 + 4 - 64 * (m_GaugeValue / FULL), 9 + x, 64 + y+1, color); //Decrease the bar height. HUD decides decrease or increase.
+	
+	if (m_DrawComponent == nullptr && m_filePath.length() != 0){
+		char *y = new char[m_filePath.length() + 1];
+		std::strcpy(y, m_filePath.c_str());
+
+		std::map<eAnimationState, Animation*>* animations = new std::map<eAnimationState, Animation*>();
+		animations->insert({ Default, new Animation(y, 2) });
+		m_DrawComponent = new DrawComponent(animations);
+		m_DrawComponent->setAnimation(Default);//set starting animation
+		//addComponent(m_DrawComponent);
+		
+	/*	drugEntity = new HUDEntity(100, 100);
+		drugEntity->addComponent(m_DrawComponent);*/
+		delete[] y;
+	}
+	else if (m_DrawComponent != nullptr && m_filePath.length() != 0){
+		//m_DrawComponent->tick(dt, this, DrawManager::getInstance().getCameraSurface()); //TODO draw drugs
+		
+		if (m_State == Gauge_Idle){
+			std::cout << "delete duh plaatjuh" << std::endl;
+			m_filePath = "";
+			delete m_DrawComponent;
+			m_DrawComponent = nullptr;
+		}
+	}
+	
+
 }
--- Overdose/Overdose/HUD.cpp
+++ Overdose/Overdose/HUD.cpp
@@ -7,18 +7,17 @@ using namespace overdose;
 HUD::HUD(GameEntity* entity, float posX, float posY)
 : m_Entity{ entity }, m_DrugEffect_ms{ INT_MAX }, m_CurrentEffect_ms{ INT_MAX }, m_Hearts{ new std::vector<HealthHearts*> }
 {
-	setPosX(posX);
-	setPosY(posY);
-	std::cout << getHealth() << std::endl;
+	setPosX(200);
+	setPosY(200);
+
 	// hearts and gauge
 	m_DrugGauge = new DrugDurationGauge(posX, posY + 20);
 	for (int i = 0; i < entity->getHealth(); i++)
 	{
-		m_Hearts->push_back(new HealthHearts(posX + i*20, posY ));
+		m_Hearts->push_back(new HealthHearts(posX + i * 20, posY));
 	}
 	m_MaxHealth = m_Entity->getHealth();
 	m_CurrentHealth = m_MaxHealth;
-	
 }
 
 HUD::~HUD()
@@ -38,11 +37,18 @@ void HUD::tick(float dt)
 	// get the data necessary
 	// set the data that it needs to draw
 	// draw
+	string filePath;
+
+	bool hasDrug = false;
 	for (std::vector<Component*>::iterator it = m_Entity->getComponentList()->begin(); it != m_Entity->getComponentList()->end(); it++)
 	{
 		if ((*it)->getComponentID() == "DrugComponent" || (*it)->getComponentID() == "XTCDrugComponent" || (*it)->getComponentID() == "SpeedDrugComponent" || (*it)->getComponentID() == "MarijuanaDrugComponent")
 		{
 			std::clock_t startTime = dynamic_cast<DrugComponent*>(*it)->getStartTime();
+			if ((*it)->getComponentID() == "SpeedDrugComponent"){
+				filePath = "assets/sprites/drug_speed_30x30.png";
+			}
+			m_DrugGauge->setFilePath(filePath);
 
 			// u need diz trev !!!!!!!!!!!!
 			int timer_end = (int)((std::clock() - startTime) / (double)(CLOCKS_PER_SEC / 1000));
@@ -53,7 +59,9 @@ void HUD::tick(float dt)
 			if (timer_end < drug_effect_ms){
 				m_DrugGauge->setGaugeValue(100 - ((float)timer_end / (float)drug_effect_ms) * 100);// decrease the drug_effect bar here
 				//std::cout << 100 - ((float)timer_end / (float)drug_effect_ms) * 100 << std::endl;
+				hasDrug = true;
 			}
+
 		}
 		else if ((*it)->getComponentID() == "NegativeXTCComponent" || (*it)->getComponentID() == "NegativeSpeedDrugComponent" || (*it)->getComponentID() == "NegativeXTCComponent")
 		{
@@ -68,29 +76,36 @@ void HUD::tick(float dt)
 			if (timer_end < drug_effect_ms){
 				m_DrugGauge->setGaugeValue(((float)timer_end / (float)drug_effect_ms) * 100);// increase the drug_effect bar here
 				//std::cout << ((float)timer_end / (float)drug_effect_ms) * 100 << std::endl;
+				hasDrug = true;
 			}
+
 		}
 		else{
 			m_DrugGauge->setState(DrugDurationGauge::Gauge_Idle); // Player has no drug
+			//m_DrugGauge->setFilePath("");
 		}
+		//m_DrugGauge->tick(dt);
+	}
 
-		m_DrugGauge->tick(dt); 
+	//if (!hasDrug){
+	//	m_DrugGauge->setState(DrugDurationGauge::Gauge_Idle); // Player has no drug
+	//}
+	m_DrugGauge->tick(dt);
 
-		//HealthHearts
-		//delayedAddHeart();
-		for (std::vector<HealthHearts*>::iterator it = m_Hearts->begin(); it != m_Hearts->end(); it++)
-		{
-			(*it)->tick(dt);
-		}
+	//HealthHearts
+	//delayedAddHeart();
+	for (std::vector<HealthHearts*>::iterator it = m_Hearts->begin(); it != m_Hearts->end(); it++)
+	{
+		(*it)->tick(dt);
+	}
 
-		//When there's 1 HUD entity in the whole game use code below
-		if (m_Entity->getHealth() >= 0 && m_Hearts->size() > m_Entity->getHealth()){
-			delete m_Hearts->back();
-			m_Hearts->pop_back();
-		}
-		else if (m_Entity->getHealth() > -1 && m_Hearts->size() < m_Entity->getHealth()){
-			isScheduledToAddHeart = true;
-		}
+	//When there's 1 HUD entity in the whole game use code below
+	if (m_Entity->getHealth() >= 0 && m_Hearts->size() > m_Entity->getHealth()){
+		delete m_Hearts->back();
+		m_Hearts->pop_back();
+	}
+	else if (m_Entity->getHealth() > -1 && m_Hearts->size() < m_Entity->getHealth()){
+		isScheduledToAddHeart = true;
 	}
 
 	// do the drawing here
