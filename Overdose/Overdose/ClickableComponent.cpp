b846179ddee97ed8329ee4b0af9ce8a2b23baad2
Huan Nguyen <Huan500@hotmail.com>
2015-01-12 00:04:16 +0100

    ClickableComponent works on levelSurface.

--- Overdose/Overdose/ClickableComponent.cpp
+++ Overdose/Overdose/ClickableComponent.cpp
@@ -1,5 +1,6 @@
 #include "ClickableComponent.h"
 #include "SoundManager.h"
+#include "LevelManager.h"
 #include <iostream>
 using namespace overdose;
 
@@ -10,10 +11,17 @@ void ClickableComponent::receive(Component *subject, ComponentMessage message, G
 void ClickableComponent::receiveMessageBatch(Component *subject, std::map<ComponentMessage, GameEntity*> messages) {}
 
 void ClickableComponent::tick(float dt, GameEntity *entity) {
+	//use these camera position, if Tutorial is on levelSurface
+	int cameraSurfaceX = LevelManager::getInstance().getCurrentLevel()->getCamera()->getPositionX();
+	int cameraSurfaceY = LevelManager::getInstance().getCurrentLevel()->getCamera()->getPositionY();
+
 	MouseClick click = InputManager::getInstance().getLastMouseClick();
 
-	if (isMouseHover(entity, click.x, click.y)) {
-
+	//(isMouseHover(entity, click.x, click.y) not needed for cameraSurface)
+	if (click.x && click.y && 
+		click.x + cameraSurfaceX < entity->getPosX() + entity->getWidth() && click.x + cameraSurfaceX > entity->getPosX() &&
+		click.y + cameraSurfaceY > entity->getPosY() && click.y + cameraSurfaceY < entity->getPosY() + entity->getHeight()) {
+		//std::cout << "clicked" << std::endl;
 		entity->broadcast(this, ClickableComponent_CLICK, entity);
 
 		SoundManager::getInstance().PlaySound(Click);
