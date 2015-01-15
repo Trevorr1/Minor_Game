a33e19d474afdbf76a1edaa624ff35a287a4b092
Antoine Koeman <akoeman1993@gmail.com>
2015-01-15 14:00:18 +0100

    removed general

--- Overdose/Overdose/Options.cpp
+++ Overdose/Overdose/Options.cpp
@@ -15,19 +15,19 @@ Options::~Options()
 
 void Options::Init()
 {
-	GameEntity *buttonGeneral = GameEntityFactory::getInstance().getGameEntity(eGameEntity::ButtonGeneral);
-	buttonGeneral->setPosX(50);
-	buttonGeneral->setPosY(10);
-	this->addEntities(buttonGeneral);
+	//GameEntity *buttonGeneral = GameEntityFactory::getInstance().getGameEntity(eGameEntity::ButtonGeneral);
+	//buttonGeneral->setPosX(50);
+	//buttonGeneral->setPosY(10);
+	//this->addEntities(buttonGeneral);
 
 	GameEntity *buttonControls = GameEntityFactory::getInstance().getGameEntity(eGameEntity::ButtonControls);
 	buttonControls->setPosX(50);
-	buttonControls->setPosY(85);
+	buttonControls->setPosY(10);
 	this->addEntities(buttonControls);
 
 	GameEntity *ButtonDrugs = GameEntityFactory::getInstance().getGameEntity(eGameEntity::ButtonDrugs);
 	ButtonDrugs->setPosX(50);
-	ButtonDrugs->setPosY(160);
+	ButtonDrugs->setPosY(85);
 	this->addEntities(ButtonDrugs);
 
 	GameEntity* button = GameEntityFactory::getInstance().getGameEntity(ButtonMainMenuGreen);
